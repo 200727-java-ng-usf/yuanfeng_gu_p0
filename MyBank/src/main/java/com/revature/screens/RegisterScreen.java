36fd68520adfc4751ff7ff07fe7d2571d353f7aa
YuanfnegGu <yuanfenggu@hotmail.com>
2020-08-25 21:47:55 -0500

    small update

--- MyBank/src/main/java/com/revature/screens/RegisterScreen.java
+++ MyBank/src/main/java/com/revature/screens/RegisterScreen.java
@@ -4,6 +4,7 @@ import com.revature.domain.Account;
 import com.revature.domain.Customer;
 import com.revature.domain.Role;
 import com.revature.repository.AccountRepo;
+import com.revature.repository.CustomerRepo;
 import com.revature.services.AccountService;
 import com.revature.services.CustomerService;
 import com.revature.util.AccountNoGenerate;
@@ -12,16 +13,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterScreen extends Screen{
 
     private CustomerService customerService;
     private AccountRepo accountRepo;
+    private CustomerRepo customerRepo;
 
     public  RegisterScreen(){
 
         customerService = new CustomerService();
         accountRepo = new AccountRepo();
+        customerRepo = new CustomerRepo();
     }
 
     @Override
@@ -36,23 +41,52 @@ public class RegisterScreen extends Screen{
             firstname = reader.readLine();
             System.out.print("Last name: ");
             lastname = reader.readLine();
+
+         // check if firstname and lastname includes numbers
+            while(HasDigit(firstname)||HasDigit(lastname)){
+                System.out.println("sorry ...");
+                System.out.println("No numbers allow in your first or last name");
+                System.out.println("please enter your name again ");
+                System.out.println();
+                System.out.print("Legal First name: ");
+                firstname = reader.readLine();
+                System.out.print("Legal Last name: ");
+                lastname = reader.readLine();
+            }
+
+
             System.out.print("User name: ");
             username = reader.readLine();
+
+            while (customerRepo.findUserByUsername(username).isPresent()==true){
+                System.out.println("username already exists");
+                System.out.println("please pick another username ");
+                System.out.println();
+                System.out.print("User name: ");
+                username = reader.readLine();
+            }
             System.out.print("Password: ");
             password = reader.readLine();
 
+
+
             Customer newCustomer = new Customer(firstname,lastname,username,password);
 
-            String newAccount = AccountNoGenerate.newAccountNo();
+
 
             /**
              * check account is the same account number in the database
+             *
+             *
              */
-            boolean hasAcc = accountRepo.findAccountByAccountNo(Integer.valueOf(newAccount)).isPresent();
+
+            String newAccount;
+            boolean hasAcc = true;
             while(hasAcc){
                 newAccount = AccountNoGenerate.newAccountNo();
+                hasAcc = accountRepo.findAccountByAccountNo(Integer.valueOf(newAccount)).isPresent(); // set new account Number for new user
             }
-            newCustomer.setAccountNo(AccountNoGenerate.newAccountNo());  // set new account Number for new user
+            newCustomer.setAccountNo(AccountNoGenerate.newAccountNo());
             newCustomer.setRole(Role.BASIC); //set up account type
 
 
@@ -75,6 +109,15 @@ public class RegisterScreen extends Screen{
 
         }
 
-
+    }
+// String checker
+    public boolean HasDigit(String content) {
+        boolean flag = false;
+        Pattern p = Pattern.compile(".*\\d+.*");
+        Matcher m = p.matcher(content);
+        if (m.matches()) {
+            flag = true;
+        }
+        return flag;
     }
 }
